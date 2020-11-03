0a1551b5109d5ca6236747de562f3dcd6664104c
Chris Hough <tech@aboutchrishough.com>
2020-11-03 07:42:58 -0700

    header navigation and formatting label sections header phone and email icons colors set, call to action image in place

--- source/assets/javascripts/vendor/fontawesome.js
+++ source/assets/javascripts/vendor/fontawesome.js
@@ -3,12 +3,14 @@ import { faHammer } from '@fortawesome/free-solid-svg-icons/faHammer';
 import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
 import { faProjectDiagram } from '@fortawesome/free-solid-svg-icons/faProjectDiagram';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+import { faPhoneSquare } from '@fortawesome/free-solid-svg-icons/faPhoneSquare';
 import { faBrain } from '@fortawesome/free-solid-svg-icons/faBrain';
 
 library.add(faBars,
             faBrain,
             faEnvelope,
             faProjectDiagram,
+            faPhoneSquare,
             faHammer);
 
 dom.watch();
