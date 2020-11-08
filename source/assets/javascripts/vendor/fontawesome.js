7da31f1fb5dc62046eb9c13a6f17985ee080ac9f
Chris Hough <tech@aboutchrishough.com>
2020-11-07 17:40:03 -0700

    add tint to background to keep color floor, organize call to action footer on primary view mobile to desktop

--- source/assets/javascripts/vendor/fontawesome.js
+++ source/assets/javascripts/vendor/fontawesome.js
@@ -1,5 +1,6 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faHammer } from '@fortawesome/free-solid-svg-icons/faHammer';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle';
 import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
 import { faProjectDiagram } from '@fortawesome/free-solid-svg-icons/faProjectDiagram';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
@@ -11,6 +12,7 @@ library.add(faBars,
             faEnvelope,
             faProjectDiagram,
             faPhoneSquare,
+            faExclamationCircle,
             faHammer);
 
 dom.watch();
