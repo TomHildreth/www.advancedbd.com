11840686668a7727367f8935e3f62c3e05649d66
Chris Hough <tech@aboutchrishough.com>
2021-01-01 18:50:47 -0700

    remove sticky footers to adjust page layout

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -8,12 +8,6 @@ const hidePreloader = () => {
   });
 };
 
-const resizeContent = () => {
-  let vh = window.innerHeight * 0.01;
-
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-};
-
 $(document).ready(() => {
   const $body = $('body');
   const $bodyId = $body.attr('id');
@@ -28,14 +22,8 @@ $(document).ready(() => {
     default:
       break;
   }
-
-  resizeContent();
 });
 
 window.addEventListener("load", function(){
   hidePreloader();
 }, false);
-
-window.addEventListener('resize', () => {
-  resizeContent();
-});
